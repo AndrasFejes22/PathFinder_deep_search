534b4e245ff082fc39c2432bdabd7331690b0249
AndrasFejes22 <afejes22@gmail.com>
2023-03-17 16:51:24 +0100

    goalNode added

--- src/pathFinder/Main.java
+++ src/pathFinder/Main.java
@@ -11,23 +11,23 @@ public class Main {
         int [][] maze =
 
                 {
-                        {1,1,1,1,1,1,1,1,1,1,1,1,1}, // start coordinate: (1,1)
+                        {1,1,1,1,1,1,1,1,1,1,1,1,1},
                         {1,0,1,0,1,0,1,0,0,0,0,0,1},
                         {1,0,1,0,0,0,1,0,1,1,1,0,1},
-                        {1,0,0,0,1,1,1,0,0,1,0,0,1},
+                        {1,0,0,0,1,1,1,0,0,0,0,0,1},
                         {1,0,1,0,0,0,0,0,1,1,1,0,1},
                         {1,0,1,0,1,1,1,0,1,0,0,0,1},
                         {1,0,1,0,1,0,0,0,1,1,1,0,1},
-                        {1,0,1,1,1,1,1,1,1,0,1,0,1},
-                        {1,0,1,9,0,0,0,0,0,0,0,0,1}, // 9: end coordinate
+                        {1,0,1,0,1,1,1,0,1,0,1,0,1},
+                        {1,0,0,0,0,0,0,0,0,0,1,0,1},
                         {1,1,1,1,1,1,1,1,1,1,1,1,1}
 
                 };
 
 
-        System.out.println();
 
-        findPath(maze, new Node(1,8));
+
+        findPath(maze, new Node(1,8), new Node(11, 8));
 
     }
 
@@ -35,14 +35,14 @@ public class Main {
         System.out.println("--------SOLUTION---------");
         for(int i = 0; i < path.size(); i++){
 
-            maze[path.get(i).y][path.get(i).x] = 2;
+            maze[path.get(i).y()][path.get(i).x()] = 2;
         }
 
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[i].length; j++) {
 
                 if(maze[i][j] == 2){
-                    System.out.print("\u001b[1;32m" + maze[i][j]+ "\u001b[0m" + " "); //greeen
+                    System.out.print("\u001b[1;32m" + maze[i][j]+ "\u001b[0m" + " "); //green
                 } else {
                     System.out.print(maze[i][j] + " ");
                 }
@@ -63,12 +63,12 @@ public class Main {
     }
 
 
-    public static void findPath(int maze[][], Node startNode){
+    public static void findPath(int maze[][], Node startNode, Node goalNode){
         int x = startNode.x;
         int y = startNode.y;
         int[][]copiedMaze = copy(maze);
         List<Node> path = new ArrayList<>();
-        if(searchPath(maze,x, y, path)){ // start coordinate: (1,1)
+        if(searchPath(maze,x, y, path, goalNode)){ // start coordinate: (x,y)
             System.out.println("There is a solution:\n");
             drawMaze(copiedMaze,path);
         } else {
@@ -91,9 +91,9 @@ public class Main {
 
     //---------------------------Searching algorithm------------------------------
 
-    public static boolean searchPath(int maze[][], int x, int y, List<Node> path){
+    public static boolean searchPath(int maze[][], int x, int y, List<Node> path, Node goalNode){
 
-
+        maze[goalNode.y][goalNode.x] = 9;
 
         if(maze[y][x] == 9){
             Node node = new Node(x, y);
@@ -104,33 +104,30 @@ public class Main {
         if(maze[y][x] == 0) {
             maze[y][x] = 2;
 
-            if (searchPath(maze, x-1, y, path)) {
+            if (searchPath(maze, x-1, y, path, goalNode)) {
                 Node node = new Node(x, y);
                 path.add(node);
                 return true;
             }
-            if (searchPath(maze, x + 1, y, path)) {
+            if (searchPath(maze, x + 1, y, path, goalNode)) {
                 Node node = new Node(x, y);
                 path.add(node);
                 return true;
             }
-            if (searchPath(maze, x, y - 1, path)) {
+            if (searchPath(maze, x, y - 1, path, goalNode)) {
                 Node node = new Node(x, y);
                 path.add(node);
                 return true;
             }
-            if (searchPath(maze, x, y + 1, path)) {
+            if (searchPath(maze, x, y + 1, path, goalNode)) {
                 Node node = new Node(x, y);
                 path.add(node);
                 return true;
             }
 
         }
-        if(path.size() == 0){
 
-        }
         return false;
-
     }
 
     // Node class
